2ac086a169f10e88e7784120012bade522ab3ebb
koda <hogehogehoge@gmail.com>
2019-02-03 18:30:12 +0900

    不要なファイルの削除

--- db/migrate/20190203091937_change_column_default_to_home.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnDefaultToHome < ActiveRecord::Migration[5.0]
-  def change
-    change_column :homes, :rent, :integer, null: false
-    change_column :homes, :age, :integer, null: false
-  end
-end
