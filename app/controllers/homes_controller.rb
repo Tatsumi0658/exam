70e35870e95ff2582a7ab13ab15cb26398e44084
koda <hogehogehoge@gmail.com>
2019-02-03 18:38:18 +0900

    不要な行の削除

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,12 +7,10 @@ class HomesController < ApplicationController
   def new
     @home = Home.new
     @home.stations.build
-    #@station = Station.new
   end
 
   def create
     @home = Home.new(home_params)
-    #@station = Station.new(st_params)
     if @home.save
       redirect_to homes_path
     else
@@ -30,9 +28,7 @@ class HomesController < ApplicationController
   def update
     if @home.update(home_params)
       redirect_to homes_path
-      flash[:success] = "更新が完了しました"
     else
-      flash[:danger] = "更新に失敗しました"
       render :edit
     end
   end
@@ -40,17 +36,12 @@ class HomesController < ApplicationController
   def destroy
     @home.destroy
     redirect_to homes_path
-    flash[:success] = "削除が完了しました"
   end
 
   private
   def home_params
     params.require(:home).permit(:name, :rent, :address, :age, :remarks, stations_attributes: [:id, :line, :station, :walk])
   end
-
-  #def st_params
-  #  params.require(:station).permit(:line, :station, :walk)
-  #end
 
   def set_homes
     @home = Home.find(params[:id])
