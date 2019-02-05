06143304c547bc480713c2efd0d8a4da94a3b5db
koda <hogehogehoge@gmail.com>
2019-02-05 22:55:22 +0900

    validates設定追加

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,6 @@
 class Station < ApplicationRecord
+  validates :line, presence: true
+  validates :station, presence: true
+  validates :walk, presence: true
   belongs_to :home, dependent: :destroy, inverse_of: :stations
 end
